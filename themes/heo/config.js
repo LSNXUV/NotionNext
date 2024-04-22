4622881fa70a4c58f1141aa366713e1994bf7cfc
LSuperNova <LSuperNova@qq.com>
2024-04-22 14:16:50 +0800

    恢复config

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: 'Share 编程',
-  HEO_HERO_TITLE_2: 'And 思维',
+  HEO_HERO_TITLE_2: 'And 思想',
   HEO_HERO_TITLE_3: 'chat.openai.com',
   HEO_HERO_TITLE_4: '船欣版本发布',
   HEO_HERO_TITLE_5: 'NextJS 14 - 更快的Turbopack',
@@ -20,7 +20,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '精选', url: '/tag/精选' },
   HEO_HERO_CATEGORY_2: { title: '热门', url: '/tag/热门' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '教程', url: '/tag/教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
